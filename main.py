7d2482d630c315ed97b99bf444b363939a6d1aa7
Yuriy Bandura <bandurayv@gmail.com>
2024-08-24 17:25:47 +0300

    Add_LSP

--- main.py
+++ main.py
@@ -1,2 +1,21 @@
 # Lesson_Cod
+class Bird():
+    def Fly(self):
+        print("Эта птица летает")
+
+
+class Duck(Bird):
+    def Fly(self):
+        print("Эта утка летает быстро")
+
+def fly_in_the_sky(animal):
+    animal.fly()
+
+
+b = Bird()
+d = Duck()
+
+fly_in_the_sky(b)
+fly_in_the_sky(d)
+
 
